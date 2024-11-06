856952c10a691b94bc1316efa66ca81e220c1f35
muhammad rofi <muhammadrofi062@gmail.com>
2024-11-06 21:18:33 +0700

    add navigation bottom

--- app/src/main/java/com/example/fe_taks_il/MainActivity.kt
+++ app/src/main/java/com/example/fe_taks_il/MainActivity.kt
@@ -7,10 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.fe_taks_il.ui.components.BottomNavigationBar
+import com.example.fe_taks_il.ui.components.Screen
+import com.example.fe_taks_il.ui.screens.CategoryScreen
+import com.example.fe_taks_il.ui.screens.HomeScreen
+import com.example.fe_taks_il.ui.screens.ProfileScreen
 import com.example.fe_taks_il.ui.theme.FE_Taks_ILTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FE_Taks_ILTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Category.route) {
+//                CategoryScreen()
+            }
+            composable(Screen.Profile.route) {
+//                ProfileScreen()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     FE_Taks_ILTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
