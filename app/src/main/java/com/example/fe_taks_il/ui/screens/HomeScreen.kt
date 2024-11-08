e7764ce88cbd0f5d10cb9b065714348dfcc6703d
muhammad rofi <muhammadrofi062@gmail.com>
2024-11-09 00:16:53 +0700

    remove not use

--- app/src/main/java/com/example/fe_taks_il/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/fe_taks_il/ui/screens/HomeScreen.kt
@@ -2,15 +2,37 @@ package com.example.fe_taks_il.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,18 +45,17 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.fe_taks_il.data.DummyData
-import com.example.fe_taks_il.models.Movies
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.fe_taks_il.R
+import com.example.fe_taks_il.data.DummyData
+import com.example.fe_taks_il.models.Movies
 import com.example.fe_taks_il.ui.theme.GreenActive
+import com.example.fe_taks_il.ui.theme.GreenLight
 import com.example.fe_taks_il.ui.theme.GreenPrimary
 import com.example.fe_taks_il.ui.theme.PoppinsBold20
 import com.example.fe_taks_il.ui.theme.PoppinsMedium12
 import com.example.fe_taks_il.ui.theme.PoppinsRegular14
-import androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors
-import androidx.navigation.NavController
-import com.example.fe_taks_il.ui.theme.GreenLight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
