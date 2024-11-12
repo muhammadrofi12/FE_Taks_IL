a1be1778df3dd11b5d81ebba374b5cd755c33dce
muhammad rofi <muhammadrofi062@gmail.com>
2024-11-12 11:57:18 +0700

    remove enableEdgeToEdge()

--- app/src/main/java/com/example/fe_taks_il/MainActivity.kt
+++ app/src/main/java/com/example/fe_taks_il/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.fe_taks_il
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -28,7 +27,6 @@ import com.example.fe_taks_il.ui.theme.FE_Taks_ILTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FE_Taks_ILTheme {
                 MainScreen()
